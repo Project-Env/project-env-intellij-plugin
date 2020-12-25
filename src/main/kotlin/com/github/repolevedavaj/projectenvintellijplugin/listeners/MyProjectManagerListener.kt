a9b922ac041b439998620f6b8867bc0832d2ff9b
GitHub Action <action@github.com>
2020-12-25 08:33:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/repolevedavaj/projectenvintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/repolevedavaj/projectenvintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.repolevedavaj.projectenvintellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.repolevedavaj.projectenvintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/repolevedavaj/projectenvintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.repolevedavaj.projectenvintellijplugin.services
+
+import com.github.repolevedavaj.projectenvintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
