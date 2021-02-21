548e10e67846b589550811ba2059363e3000ce18
repolevedavaj <jonas.bamberger@gmail.com>
2021-02-21 11:13:58 +0100

    added test case to assert that clean does not reload config

--- src/test/kotlin/io/projectenv/intellijplugin/services/ProjectEnvServiceIT.kt
+++ src/test/kotlin/io/projectenv/intellijplugin/services/ProjectEnvServiceIT.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.apache.commons.io.FileUtils
 import org.apache.commons.io.IOUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.idea.maven.project.MavenProjectsManager
@@ -26,6 +27,27 @@ class ProjectEnvServiceIT : BasePlatformTestCase() {
         assertMavenSettings()
         assertJdkSettings()
         assertNodeSettings()
+    }
+
+    @Test
+    fun testCleanDoesNotReloadConfig() {
+        val config = copyResourceToProjectRoot("project-env.yml")
+
+        val service = myFixture.project.service<ProjectEnvService>()
+        service.refreshProjectEnv()
+
+        val configurationService = myFixture.project.service<ProjectEnvConfigurationService>()
+        assertThat(configurationService.getConfiguration()).isNotNull()
+
+        assertThat(config.delete()).isTrue()
+        assertThat(config).doesNotExist()
+
+        service.cleanProjectEnv()
+
+        assertThat(configurationService.getConfiguration()).isNotNull()
+
+        configurationService.refresh()
+        assertThat(configurationService.getConfiguration()).isNull()
     }
 
     override fun tearDown() {
@@ -55,19 +77,23 @@ class ProjectEnvServiceIT : BasePlatformTestCase() {
 
     private fun assertToolPath(path: String?) {
         assertThat(path).startsWith(getProjectRoot().canonicalPath)
-        assertThat(File(path)).exists()
+        assertThat(File(path!!)).exists()
     }
 
-    private fun copyResourceToProjectRoot(resource: String) {
+    private fun copyResourceToProjectRoot(resource: String): File {
         val resultingFile = File(getProjectRoot(), resource)
+        FileUtils.forceMkdirParent(resultingFile)
+
         javaClass.getResourceAsStream(resource).use { inputStream ->
             FileOutputStream(resultingFile).use { outputStream ->
                 IOUtils.copy(inputStream, outputStream)
             }
         }
+
+        return resultingFile
     }
 
     private fun getProjectRoot(): File {
-        return File(myFixture.project.basePath)
+        return File(myFixture.project.basePath!!)
     }
 }
